eab56fbc5a3466da8a4036e3397e1bfe37c67d70
2MohamedHssan <Mh3523976@gmail.com>
2024-12-24 20:34:30 +0200

    first commit

---
+++ config/middlewares.js
@@ -0,0 +1,31 @@
+module.exports = [
+  'strapi::logger',
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults:true,
+        directives: {
+          'connect-src':["'self'",'https:'],
+          'img-src':["'self'",'data','blob','market-assets.strapi.io','res.cloudinay.com'],
+          'media-src':[
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'res.cloudinay.com',
+          ],
+          upgradeInsecureRequests:null,
+        }
+      }
+    }
+  },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
